e067be3c9b0b27ee02fc370ee5774569f60603f4
Evgeny <ew.silin@gmail.com>
2023-02-02 20:54:54 +0300

    Add onMouse film

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const MovieCard = (props) => {
-  const { film: { name, img } } = props;
+  const { film: { name, img, video }, onCLick } = props;
+  const videoRef = useRef(null);
+  let timer;
+  const onOver = () => {
+    timer = setTimeout(() => {
+      videoRef.current.play();
+    }, 1000)
+  };
+  const onOut = () => {
+    timer = clearTimeout(timer);
+    videoRef.current.load();
+  };
   return (
-    <article className="small-movie-card catalog__movies-card">
+    <article
+      className="small-movie-card catalog__movies-card"
+      onClick={(e) => onCLick(e, e.target)}
+      onMouseOver={() => onOver()}
+      onMouseOut={() => onOut()}
+    >
       <div className="small-movie-card__image">
-        <img src={img}
-          alt={name} width="280" height="175" />
+        <video
+          width="280"
+          height="175"
+          poster={img}
+          muted="muted"
+          ref={videoRef}
+        >
+          <source src={video} />
+        </video>
       </div>
       <h3 className="small-movie-card__title">
         <a className="small-movie-card__link" href="movie-page.html">{name}</a>
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MovieCard from '../MovieCard/MovieCard.jsx';
 
 const MovieList = (props) => {
+  const [film, setFilm] = useState();
+  const onCLickButton = (e, film) => {
+    e.preventDefault;
+    setFilm(film);
+  };
   const { films } = props;
+
   return (
     <div className="catalog__movies-list">
       {films.map((film, i) => {
@@ -10,11 +16,12 @@ const MovieList = (props) => {
           <MovieCard
             key={film.name + i}
             film={film}
+            onCLick={(e) => onCLickButton(e, film)}
           />
         )
       })}
     </div>
-  );
-}
+  )
+};
 
 export default MovieList;
